388b3b375e0c9d7bad8a1a3683c1a4fd7b877567
Javier Luque <javluqrui@alum.us.es>
2026-04-09 11:53:15 +0200

    feat: estructura de mercado y función 'listFotSale'

--- contracts/TraDeckNFT.sol
+++ contracts/TraDeckNFT.sol
@@ -19,7 +19,33 @@ contract TraDeckNFT is ERC721URIStorage {
         return tokenId;
     }
 
-    
+    //2: Estructura del Mercado
+    enum TradeState {NotListed, Listed, InEscrow}
+
+    struct Trade {
+        address seller;
+        address buyer;
+        uint256 price;
+        TradeState state;
+    }
+
+    mapping(uint256 => Trade) public trades;
+
+    function ListForSale(uint256 tokenId, uint256 price) public {
+        require(ownerOf(tokenId) == msg.sender, "Solo el propietario puede listar la carta");
+        require(price > 0, "El precio debe ser mayor a cero");
+        require(trades[tokenId].state == TradeState.NotListed, "La carta ya esta listada");
+
+        _transfer(msg.sender, address(this), tokenId);
+
+        trades[tokenId] = Trade({
+            seller: msg.sender,
+            buyer: address(0),
+            price: price,
+            state: TradeState.Listed
+        });
+
+    }
 
 
 }
